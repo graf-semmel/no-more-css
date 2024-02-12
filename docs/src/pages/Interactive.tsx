1e166665054819f21e2feccf1e258f1ccfd33aaa
Tristan Heinig <graf.semmel@googlemail.com>
2024-02-12 22:14:04 +1100

    working on button styles

--- docs/src/pages/Interactive.tsx
+++ docs/src/pages/Interactive.tsx
@@ -31,15 +31,54 @@ function Interactive() {
             <row class="gap-2">
               <button>primary</button>
               <button class="secondary">secondary</button>
-              <button class="base">base</button>
-              <button class="surface">surface</button>
               <button class="success">success</button>
               <button class="error">error</button>
+              <button class="base">base</button>
             </row>
           </main>
           <pre>
             <Code>
-              <button>Click me</button>
+              <button>primary</button>
+              <button class="secondary">secondary</button>
+              <button class="success">success</button>
+              <button class="error">error</button>
+              <button class="base">base</button>
+            </Code>
+          </pre>
+          <main>
+            <row class="gap-2">
+              <button class="XS">extra small</button>
+              <button class="S">small</button>
+              <button>normal</button>
+              <button class="L">large</button>
+              <button class="XL">extra large</button>
+            </row>
+          </main>
+          <pre>
+            <Code>
+              <button class="XS">extra small</button>
+              <button class="S">small</button>
+              <button>normal</button>
+              <button class="L">large</button>
+              <button class="XL">extra large</button>
+            </Code>
+          </pre>
+          <main>
+            <row class="gap-2">
+              <button class="outline">primary</button>
+              <button class="secondary outline">secondary</button>
+              <button class="success outline">success</button>
+              <button class="error outline">error</button>
+              <button class="base outline">base</button>
+            </row>
+          </main>
+          <pre>
+            <Code>
+              <button class="outline">primary</button>
+              <button class="secondary outline">secondary</button>
+              <button class="success outline">success</button>
+              <button class="error outline">error</button>
+              <button class="base outline">base</button>
             </Code>
           </pre>
         </card>
