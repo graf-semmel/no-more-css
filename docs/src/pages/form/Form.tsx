d28e9038c5c96a01d767aee0b9f27104256d1660
Tristan Heinig <tristan@littlepay.com>
2024-04-15 20:36:57 +1000

    improve input styling

--- docs/src/pages/form/Form.tsx
+++ docs/src/pages/form/Form.tsx
@@ -40,30 +40,85 @@ export default function Form() {
           for the
           <code>fieldset</code> element.
         </p>
-      </section>
-      <ElementPreview
-        html={
-          <form>
-            <fieldset>
-              <legend>Sign up</legend>
-              <label for="name">
-                Name
-                <input id="name" type="text" placeholder="Name" />
-              </label>
-              <label for="email">
-                Email
-                <input id="email" type="email" placeholder="your@email.com" />
-              </label>
-            </fieldset>
-            <button type="submit">Submit</button>
-          </form>
-        }
-        css={`
-          h2 {
-            color: red;
+        <ElementPreview
+          html={
+            <form>
+              <fieldset>
+                <legend>Sign up</legend>
+                <label for="name">
+                  Name
+                  <input id="name" type="text" placeholder="Name" />
+                </label>
+                <label for="email">
+                  Email
+                  <input id="email" type="email" placeholder="your@email.com" />
+                </label>
+              </fieldset>
+              <button type="submit">Submit</button>
+            </form>
           }
-        `}
-      />
+          css={`
+            h2 {
+              color: red;
+            }
+          `}
+        />
+      </section>
+      <section>
+        <h2>Input</h2>
+        <p>
+          The <code>input</code> element is used to create form controls. The
+          type attribute specifies the type of input element to display.
+        </p>
+        <ElementPreview
+          html={
+            <form>
+              <input type="text" placeholder="text" />
+              <input type="password" placeholder="password" />
+              <input type="email" placeholder="email" />
+              <input type="number" placeholder="number" />
+              <input type="tel" placeholder="tel" />
+              <input type="url" placeholder="url" />
+              <input type="search" placeholder="search" />
+            </form>
+          }
+        />
+      </section>
+      <section>
+        <h2>Search</h2>
+        <p>
+          The <code>search</code> input type is used to create a text field for
+          entering a search string.
+        </p>
+        <ElementPreview
+          html={
+            <form>
+              <input type="search" placeholder="search" />
+            </form>
+          }
+        />
+      </section>
+      <section>
+        <h2>Checkbox</h2>
+        <p>
+          The <code>checkbox</code> input type is used to create a checkbox
+          control.
+        </p>
+        <ElementPreview
+          html={
+            <form>
+              <label for="checkbox">
+                Checkbox 1
+                <input id="checkbox 1" type="checkbox" />
+              </label>
+              <label for="checkbox">
+                Checkbox 2
+                <input id="checkbox 2" type="checkbox" />
+              </label>
+            </form>
+          }
+        />
+      </section>
       <Tabs
         items={[
           { label: "Tab 1", content: <p>Hello World from Tab 1!</p> },
