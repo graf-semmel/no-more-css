153db97ee714c5ff84f8f2fcef25415ac425eece
Tristan <graf.semmel@googlemail.com>
2025-07-06 01:08:14 +1000

    work on landing page

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,47 +1,11 @@
-import Code from "./Code";
-
 function App() {
   return (
     <div class="container blog">
-      <section style="background: #eee">
-        <h2>Button</h2>
-        <Code>
-          <button>Default</button>
-        </Code>
-        <button>Default</button>
-      </section>
-      <section style="background: #eee">
-        <h2>Typography</h2>
-      </section>
-      <section style="background: #eee">
-        <h2>Figure</h2>
-      </section>
-      <section style="background: #eee">
-        <h2>Code</h2>
-        <Code>
-          <button>Default</button>
-        </Code>
-      </section>
-      <section style="background: #eee">
-        <h2>Cards</h2>
-        <card>
-          <header>
-            <h1>Home</h1>
-          </header>
-          <main>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              voluptatum, quibusdam, voluptate, quia quod quos voluptas
-              voluptatem quas quidem doloribus quae. Quisquam voluptatum,
-              quibusdam, voluptate, quia quod quos voluptas voluptatem quas
-              quidem doloribus quae.
-            </p>
-          </main>
-          <footer>
-            <button>Click me</button>
-          </footer>
-        </card>
-      </section>
+      <h1 class="uppercase">
+        No More
+        <br />
+        <mark class="secondary text-8xl inline-block">CSS</mark>
+      </h1>
     </div>
   );
 }
